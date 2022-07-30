01cfc3f90117b426c1d8f69ac3ed6763cbc65a57
emysdias <diassousa@yahoo.com.br>
2022-07-30 17:02:45 -0300

    change file to test git

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <wiringPi.h>
 
-int SEMAFORO_1_VERDE = 8;
+int SEMAFORO_1_VERDE = 31;
 int SEMAFORO_1_AMARELO = 9;
 int SEMAFORO_1_VERMELHO = 14;
-int SEMAFORO_2_VERDE = 30;
+int SEMAFORO_2_VERDE = 28;
 int SEMAFORO_2_AMARELO = 21;
 int SEMAFORO_2_VERMELHO = 22;
 int BOTAO_PEDESTRE_1 = 12;
@@ -20,12 +20,18 @@ int main(void)
     wiringPiSetup();
 
     pinMode(SEMAFORO_1_VERDE, OUTPUT);
+    pinMode(SEMAFORO_2_VERDE, OUTPUT);
     while (1)
     {
         digitalWrite(SEMAFORO_1_VERDE, HIGH);
         delay(500);
         digitalWrite(SEMAFORO_1_VERDE, LOW);
         delay(500);
+        digitalWrite(SEMAFORO_2_VERDE, HIGH);
+        delay(500);
+        digitalWrite(SEMAFORO_2_VERDE, LOW);
+        delay(500);
     }
+        
     return 0;
 }
