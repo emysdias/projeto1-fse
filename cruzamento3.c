a765a3c35a76518f60f11ea3d2935ee32c81ea07
emysdias <diassousa@yahoo.com.br>
2022-08-01 23:31:21 -0300

    ajuste do semaforo

--- cruzamento3.c
+++ cruzamento3.c
@@ -29,10 +29,10 @@ int BOTAO_PEDESTRE_1 = 10;
 int BOTAO_PEDESTRE_2 = 11;
 int SENSOR_PASSAGEM_1 = 15;
 int SENSOR_PASSAGEM_2 = 16;
-int SENSOR_VELOCIDADE_1_A = 1;
-int SENSOR_VELOCIDADE_1_B = 4;
-int SENSOR_VELOCIDADE_2_A = 5;
-int SENSOR_VELOCIDADE_2_B = 6;
+int SENSOR_VELOCIDADE_1_A = 1; // para carro 1
+int SENSOR_VELOCIDADE_1_B = 4; // passa carro 1 e o para tbm
+int SENSOR_VELOCIDADE_2_A = 5; // para carro 2
+int SENSOR_VELOCIDADE_2_B = 6; // passa carro 2 e o para tbm
 
 typedef struct led
 {
@@ -57,11 +57,12 @@ void passengerPass()
 
     if (interruptTime - lastInterruptTime > 300)
     {
-        if (auxiliary.green.state)
+        if (auxiliary.green.state && digitalRead(BOTAO_PEDESTRE_1))
         {
             pressedButtonOnGreen1 = true;
         }
-        else if (principal.green.state)
+
+        else if (principal.green.state && digitalRead(BOTAO_PEDESTRE_2))
         {
             pressedButtonOnGreen2 = true;
         }
@@ -69,7 +70,7 @@ void passengerPass()
     lastInterruptTime = interruptTime;
 }
 
-void carPass()
+void carPassSensor()
 {
     long interruptTime = millis();
 
@@ -173,7 +174,7 @@ void setTimer()
     delay(1000);
     setTimerLeds(PRIMEIRO_ESTADO_SEMAFORO); // primeiro estado semaforo
     delay(10000);
-    checkTimeTraffic(5);
+    checkTimeTraffic(8);
     setTimerLeds(SEGUNDO_ESTADO_SEMAFORO);
     delay(3000);
     setTimerLeds(ESTADO_VERMELHO_SEMAFORO);
@@ -198,7 +199,7 @@ led setLed(int num, int pin)
 void setTrafficLights()
 {
     principal.green = setLed(1, SEMAFORO_2_VERDE);    // 1 = verde
-    principal.yellow = setLed(2, SEMAFORO_2_AMARELO); // 2 = amareuxiliarylo
+    principal.yellow = setLed(2, SEMAFORO_2_AMARELO); // 2 = amarelo
     principal.red = setLed(3, SEMAFORO_2_VERMELHO);   // 3 = vermelho
 
     auxiliary.green = setLed(1, SEMAFORO_1_VERDE);    // 1 = verde
@@ -222,11 +223,15 @@ int main(void)
 
     pinMode(SENSOR_PASSAGEM_1, INPUT);
     pullUpDnControl(SENSOR_PASSAGEM_1, PUD_UP);
-    wiringPiISR(SENSOR_PASSAGEM_1, INT_EDGE_BOTH, &carPass);
+    wiringPiISR(SENSOR_PASSAGEM_1, INT_EDGE_BOTH, &carPassSensor);
 
     pinMode(SENSOR_PASSAGEM_2, INPUT);
     pullUpDnControl(SENSOR_PASSAGEM_2, PUD_UP);
-    wiringPiISR(SENSOR_PASSAGEM_2, INT_EDGE_BOTH, &carPass);
+    wiringPiISR(SENSOR_PASSAGEM_2, INT_EDGE_BOTH, &carPassSensor);
+
+    // pinMode(SENSOR_VELOCIDADE_1_A, INPUT);
+    // pullUpDnControl(SENSOR_VELOCIDADE_1_A, PUD_UP);
+    // wiringPiISR(SENSOR_VELOCIDADE_1_A, INT_EDGE_FALLING, &teste);
 
     while (1)
     {
